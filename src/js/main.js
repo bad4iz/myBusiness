838efae092c94835f0610aef43d84a5f82cefc19
Евгений Плотников <bad4iz@gmail.com>
2017-02-09 10:46:40 +0400

    первые шаги

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
+var Person = function (config) {
+    this.name = config.name;
+    this.age = config.age;
+    this.job = config.job;
+    };
 
+var nic = new Person({name:'Николя', age:'25', job:'супер работник'});
