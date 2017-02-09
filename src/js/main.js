fe8ae61a53da8ab03b38707fd36247bc943a7093
bad4iz <bad4iz@gmail.com>
2017-02-09 21:28:10 +0400

    а

--- src/js/main.js
+++ src/js/main.js
@@ -1,74 +1,94 @@
-// Модель человека
-var Person;
-Person = Backbone.Model.extend({
-    defaults: {
-        names: 'Вася',
-        age: '20',
-        job: 'супер- пупeр программист'
-    }
-});
+(function () {
+    window.App = {
+        Models: {},
+        Views: {},
+        Collection: {}
+    };
 
-var person = new Person();
+    // хелпер шаблона
+    window.template = function (id) {
+        return _.template($('#' + id).html());
+    };
+
+    // Модель человека
+    App.Models.Person = Backbone.Model.extend({
+        defaults: {
+            names: 'Вася',
+            age: '20',
+            job: 'супер- пупeр программист'
+        }
+    });
+
+    var person = new App.Models.Person();
 
 // Список людей (колекция)
-var PeopleCollection = Backbone.Collection.extend({
-    model: Person
-});
+    App.Collection.People = Backbone.Collection.extend({
+        model: App.Models.Person
+    });
 
 // вид представление списка людей
-var PeopleView = Backbone.View.extend({
-    tagName:'ul',
-    initialize: function () {
-        // console.info(this.collection);
-    },
-    render: function () {
-        // 1 пройтись пр всему списку и срендерить каждый PresonView
-        this.collection.each(function (person) {
-            var personView = new PersonView({model: person});
-            this.$el.append(personView.el);
-            console.log(this.collection);
-        }, this);
+    App.Views.People = Backbone.View.extend({
+        tagName: 'ul',
+        initialize: function () {
+            // console.info(this.collection);
+        },
+        render: function () {
+            // 1 пройтись пр всему списку и срендерить каждый PresonView
+            this.collection.each(function (person) {
+                var personView = new App.Views.Person({model: person});
+                this.$el.append(personView.render().el);
+                // console.log(this.collection);
+            }, this);
 
-        // 2 вставить в главный тег ul (this.$el)
+            return this;
+            // 2 вставить в главный тег ul (this.$el)
 
-    }
+        }
 
-});
+    });
 
-// вид представления одного человека
-var PersonView = Backbone.View.extend({
-    tagName: 'li',
-    template: _.template($('#person-id').html()),
-    initialize: function () {
-        // при инициализации
-        this.render();
-    },
-    render: function () {
-        // антипатерн
-        // this.$el.html(this.model.get('name') + ' (' + this.model.get('age') + ') - ' + this.model.get('job') );
+    // вид представления одного человека
+    App.Views.Person = Backbone.View.extend({
+        tagName: 'li',
 
-        this.$el.html(this.template(this.model.toJSON()));
+        template: template('person-id'),
 
-    }
-});
+        initialize: function () {
+            // при инициализации
+            this.render();
+        },
+
+        render: function () {
+            // антипатерн
+            // this.$el.html(this.model.get('name') + ' (' + this.model.get('age') + ') - ' + this.model.get('job') );
+
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
 
 // заглушка прихода данных с сервера
-var people = [
-    {
-        name: 'Петя',
-        age: 21,
-        job: 'Сварщик'
-    },
-    {
-        name: 'Анна',
-        age: 18,
-        job: 'бухгалтер'
-    },
-    {
-        name: 'Вася',
-        age: 16,
-        job: 'Супур-пупер программист'
-    }
-];
-var peopleCollection = new PeopleCollection(people);
-var peopleView = new PeopleView({collection: peopleCollection});
+    var people = [
+        {
+            name: 'Петя',
+            age: 21,
+            job: 'Сварщик'
+        },
+        {
+            name: 'Анна',
+            age: 18,
+            job: 'бухгалтер'
+        },
+        {
+            name: 'Вася',
+            age: 16,
+            job: 'Супур-пупер программист'
+        }
+    ];
+    var peopleCollection = new  App.Collection.People(people);
+    var peopleView = new App.Views.People({collection: peopleCollection});
+
+    $(document.body).append(peopleView.render().el);
+
+
+}());
