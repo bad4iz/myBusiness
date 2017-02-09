f1fbf195022ba8b10b208ddbea676994b57cd076
bad4iz <bad4iz@gmail.com>
2017-02-09 21:37:13 +0400

    12

--- src/js/main2.js
+++ src/js/main2.js
@@ -1,6 +1,18 @@
 (function () {
    // пространство имен
     window.App = {
-        Models: {}
+        Models: {},
+        Collections:{},
+        Views:{}
     };
+
+    //шаблон
+    window.template = function (id) {
+        return _.template($('#' + id).html());
+    };
+
+    App.Models.Task = Backbone.Model.extend({
+
+    });
+    App.Views.Task = Backbone.Views.extend({});
 }());
