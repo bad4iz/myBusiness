29a33d96a5decc0a958bc81c9da9f56cd10d127a
Евгений Плотников <bad4iz@gmail.com>
2017-02-09 12:33:47 +0400

    первые шаги

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,44 @@
-var Person = function (config) {
-    this.name = config.name;
-    this.age = config.age;
-    this.job = config.job;
-    };
+var Person;
+Person = Backbone.Model.extend({
+    defaults: {
+        names: 'Вася',
+        age: '20',
+        job: 'супер- пупeр программист'
+    },
+    validate: function (attrs) {
+        if (attrs.age <= 0) {
+            return "Возраст должен быть положительный";
+        }
+        if (!attrs.name) {
+            return "имя уже сушествует";
+        }
+    },
+    walk: function () {
+        return this.get('names') + ' is walking';
+    }
+});
 
-var nic = new Person({name:'Николя', age:'25', job:'супер работник'});
+
+
+PersonView = Backbone.View.extend({
+    tagName: 'li',
+    template:_.template( $('#person-id').html() ),
+    initialize: function () {
+            // при инициализации
+    },
+    render: function () {
+        // антипатерн
+        // this.$el.html(this.model.get('name') + ' (' + this.model.get('age') + ') - ' + this.model.get('job') );
+
+        this.$el.html(this.template(this.model.toJSON()));
+
+    }
+});
+
+var person = Person();
+
+person.on('error',function (model, error) {
+    console.error(error);
+});
+
+var personView = new PersonView({model: person});
