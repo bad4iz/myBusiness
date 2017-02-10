3e445efd68fc350fd45869eddfbcd99a614cf581
Евгений Плотников <bad4iz@gmail.com>
2017-02-10 14:36:18 +0400

    15

--- src/js/main2.js
+++ src/js/main2.js
@@ -12,21 +12,36 @@ $(function () {
     };
 
     App.Models.Task = Backbone.Model.extend({});
+    // одно отображение
     App.Views.Task = Backbone.View.extend({
+        initialize: function () {
+            this.model.on('change', this.render, this);
+        },
         tagName: 'li',
+        template: template('taskTemplate'),
         render: function() {
-            this.$el.html( this.model.get('title') );
+            var template = this.template(this.model.toJSON());
+            this.$el.html( template );
             return this;
+        },
+        events:{
+            'click .edit': 'editTask'
+        },
+        editTask: function () {
+            // console.log('изменение задачи');
+           var newTaskTitle = prompt('как обозвать задачу', this.model.get('title'));
+           this.model.set('title', newTaskTitle);
         }
     });
 
     App.Collections.Task = Backbone.Collection.extend({
         model: App.Models.Task
     });
+
     App.Views.Tasks = Backbone.View.extend({
         tagName: 'ul',
         render: function () {
-            this.collection.each(this.assOne, this);
+            this.collection.each(this.addOne, this);
             return this;
         },
         addOne: function (task) {
@@ -37,22 +52,21 @@ $(function () {
         }
     });
 
-    var task = [
+    var tasksCollection = new App.Collections.Task([
         {
-            title: ' sdfadf'
+            title: 'киношка'
         },
         {
-            title: ' sdfadf'
+            title: 'игрулька'
         },
         {
-            title: ' sdfadf'
+            title: 'закежка'
         }
-    ];
-
-    var tasks = new App.Collections.Task({collection: task});
+    ]);
 
 
-    $('body').append(taskView.render().el);
+    var tasksView = new App.Views.Tasks({collection: tasksCollection});
+    $('.tasks').html(tasksView.render().el);
 
 });
 
