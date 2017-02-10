c2a1f3422ff978f32925d6f10943949c69e9b7b2
bad4iz <bad4iz@gmail.com>
2017-02-10 07:15:40 +0400

    13

--- src/js/main2.js
+++ src/js/main2.js
@@ -1,12 +1,12 @@
 $(function () {
-    // пространство имен
-    window.App = {
-        Models: {},
-        Collections: {},
-        Views: {}
+    //
+    window.App ={
+        Models:{},
+        Collections:{},
+        Views:{}
     };
 
-    //шаблон
+    //
     window.template = function (id) {
         return _.template($('#' + id).html());
     };
@@ -19,40 +19,12 @@ $(function () {
             return this;
         }
     });
-    App.Collections.Task = Backbone.Collection.extend({
-        model: App.Models.Task
+
+    var task = new App.Models.Task({
+        title: "Сходить в магазин",
+        priority: 4
     });
-    App.Views.Task = Backbone.View.extend({
-        tagName: 'ul',
-        render: function () {
-            this.collection.each(this.addOne, this);
-            return this;
-        },
-        addOne: function (task) {
-            //создавать новый дочернии вид
-            var taskView = new App.Views.Task({model: task});
+    var taskView = new App.Views.Task({model: task});
 
-            // добавлять его в корневой элемент
-            this.$el.append(taskView.render().el);
-        }
-    });
-
-
-    var tasksCollection = new App.Collections.Task([
-        {
-            title: "выучить джавускрипт",
-            priority: 4
-        },
-        {
-            title: "выучить backbone",
-            priority: 4
-        }
-    ]);
-    //
-    // console.info(taskView.render().el);
-
-    var tasksView = new App.Views.Task({collection: tasksCollection});
-
-    tasksView.render();
-    $('body').html(tasksView.el);
+    console.log(taskView.render().el);
 });
