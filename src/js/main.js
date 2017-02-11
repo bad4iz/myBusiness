64aa93cc64ea67b80dd026e1a5fec9a4f02257d5
bad4iz <bad4iz@gmail.com>
2017-02-11 11:49:17 +0400

    route

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
         Models: {},
         Views: {},
         Collection: {},
-        Routs:{}
+        Routs: {}
     };
 
     // хелпер шаблона
@@ -11,85 +11,33 @@
         return _.template($('#' + id).html());
     };
 
-    // Модель человека
-    App.Models.Person = Backbone.Model.extend({
-        defaults: {
-            names: 'Вася',
-            age: '20',
-            job: 'супер- пупeр программист'
-        }
-    });
+    /////////////////////////////////////////////
+    // роутер
+    ////////////////////////////////////////////
+    App.Routers.Main =
+        var Controller = Backbone.Router.extend({
+            routes: {
+                "": "index", // Пустой hash-тэг
+                "#": "index", // Начальная страница
+                "contacts": "contacts", // Блок удачи
+                "other": "other" // Блок ошибки
+            },
 
-    var person = new App.Models.Person();
+            index: function () {
+                $(".block").hide(); // Прячем все блоки
+                $("#index").show(); // Показываем нужный
+            },
 
-// Список людей (колекция)
-    App.Collection.People = Backbone.Collection.extend({
-        model: App.Models.Person
-    });
+            contacts: function () {
+                $(".block").hide();
+                $("#contacts").show();
+            },
 
-// вид представление списка людей
-    App.Views.People = Backbone.View.extend({
-        tagName: 'ul',
-        initialize: function () {
-            // console.info(this.collection);
-        },
-        render: function () {
-            // 1 пройтись пр всему списку и срендерить каждый PresonView
-            this.collection.each(function (person) {
-                var personView = new App.Views.Person({model: person});
-                this.$el.append(personView.render().el);
-                // console.log(this.collection);
-            }, this);
-
-            return this;
-            // 2 вставить в главный тег ul (this.$el)
-
-        }
-
-    });
-
-    // вид представления одного человека
-    App.Views.Person = Backbone.View.extend({
-        tagName: 'li',
-
-        template: template('person-id'),
-
-        initialize: function () {
-            // при инициализации
-            this.render();
-        },
-
-        render: function () {
-            // антипатерн
-           // this.$el.html(this.model.get('name') + ' (' + this.model.get('age') + ') - ' + this.model.get('job') );
-
-            this.$el.html(this.template(this.model.toJSON()));
-            return this;
-        }
-    });
-
-// заглушка прихода данных с сервера
-    var people = [
-        {
-            name: 'Петя',
-            age: 21,
-            job: 'Сварщик'
-        },
-        {
-            name: 'Анна',
-            age: 18,
-            job: 'бухгалтер'
-        },
-        {
-            name: 'Вася',
-            age: 16,
-            job: 'Супур-пупер программист'
-        }
-    ];
-    var peopleCollection = new  App.Collection.People(people);
-    var peopleView = new App.Views.People({collection: peopleCollection});
-
-    $('body').append(peopleView.render().el);
+            other: function () {
+                $(".block").hide();
+                $("#other").show();
+            }
+        });
 
 
 }());
