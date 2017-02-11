c08594f262ee3ff1fc2058fa1621a802e2d0e36d
bad4iz <bad4iz@gmail.com>
2017-02-11 16:16:27 +0400

    не повтораяется

--- src/js/main.js
+++ src/js/main.js
@@ -127,12 +127,12 @@
         }
     ];
 
-    var contactCollection = new Contacts(persons);
-
-    var contactsView = new ContactsView({
-        collection: contactCollection
-    });
-
+//    var contactCollection = new Contacts(persons);
+//
+//    var contactsView = new ContactsView({
+//        collection: contactCollection
+//    });
+//
 
 
     /////////////////////////////////////////////
@@ -149,12 +149,18 @@
         index: function () {
             $(".block").hide(); // Прячем все блоки
             $("#index").show(); // Показываем нужный
+            //            contactsView.model.destroy();
         },
 
         contacts: function () {
             $(".block").hide();
             $("#contacts").show();
+//            contactsView.model.destroy();
+            var contactCollection = new Contacts(persons);
 
+            var contactsView = new ContactsView({
+                collection: contactCollection
+            });
 
             $('#list-contacts').html(contactsView.render().el);
         },
