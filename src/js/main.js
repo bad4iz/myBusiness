b68297c60d3cef7fd6161204a6b27b8dfae9d94d
bad4iz <bad4iz@gmail.com>
2017-02-11 13:42:57 +0400

    контакты

--- src/js/main.js
+++ src/js/main.js
@@ -15,32 +15,93 @@
     // роутер
     ////////////////////////////////////////////
     var Controller = Backbone.Router.extend({
-            routes: {
-                "": "index", // Пустой hash-тэг
-                "#": "index", // Начальная страница
-                "contacts": "contacts", // Блок контактов
-                "other": "other" // Блок другой
-            },
+        routes: {
+            "": "index", // Пустой hash-тэг
+            "#": "index", // Начальная страница
+            "contacts": "contacts", // Блок контактов
+            "other": "other" // Блок другой
+        },
 
-            index: function () {
-                $(".block").hide(); // Прячем все блоки
-                $("#index").show(); // Показываем нужный
-            },
+        index: function () {
+            $(".block").hide(); // Прячем все блоки
+            $("#index").show(); // Показываем нужный
+        },
 
-            contacts: function () {
-                $(".block").hide();
-                $("#contacts").show();
-            },
+        contacts: function () {
+            $(".block").hide();
+            $("#contacts").show();
+        },
 
-            other: function () {
-                $(".block").hide();
-                $("#other").show();
-            }
-        });
+        other: function () {
+            $(".block").hide();
+            $("#other").show();
+        }
+    });
 
     var controller = new Controller(); // Создаём контроллер
 
     Backbone.history.start(); // Запускаем HTML5 History push
 
+    //////////////////////////////////
+    //  наблюдение за менюшкой
+    //////////////////////////////////
+    var Start = Backbone.View.extend({
+        el: $("nav"), // DOM элемент widget'а
+        events: {
+            "click .index": "index", // Обработчик клика  "index"
+            "click .contacts": "contacts", // Обработчик клика  "index"
+            "click .other": "other" // Обработчик клика  "index"
+        },
+        index: function () {
+            controller.navigate("", true); // переход на страницу
+        },
+        contacts: function () {
+            controller.navigate("contacts", true); // переход на страницу
+        },
+        other: function () {
+            controller.navigate("other", true); // переход на страницу
+        },
+    });
+
+    var start = new Start();
+
+    /////////////////////////////////
+    //  model person
+    /////////////////////////////////
+    var Person = Backbone.Model.extend({
+        defaults: {
+            name: 'Dima',
+            age: 23,
+            occupation: 'web developer'
+        }
+    });
+
+    /////////////////////////////////
+    //  вид person
+    /////////////////////////////////
+
+
+    var PersonView = Backbone.View.extend({
+        tagName: 'li',
+
+        template: template('person-id') ,
+
+        initialize: function () {
+            this.render();
+        },
+
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
+
+    var person = new Person;
+    var personView = new PersonView({
+        model: person
+    });
+
+
+    $('#contacts').append(personView.render().el);
 
 }());
