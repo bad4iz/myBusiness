eb68c209a6b710646d4f4a829ffc74b8f45de6ec
bad4iz <bad4iz@gmail.com>
2017-02-13 18:06:10 +0400

    create models files

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,30 @@
-(function () {
-    window.App = {
-        Models: {},
-        Views: {},
-        Collections: {},
-        Router: {}
-    };
+// (function () {
+//     window.App = {
+//         Models: {},
+//         Views: {},
+//         Collections: {},
+//         Router: {}
+//     };
+//
+//     // хелпер шаблона
+//     window.template = function (id) {
+//         return _.template($('#' + id).html());
+//     };
+//
+// }());
 
-    // хелпер шаблона
-    window.template = function (id) {
-        return _.template($('#' + id).html());
-    };
 
-}());
+require.config({
+    paths: {
+        jquery: "lib/jquery-1.8.3",
+        underscore: 'lib/underscore',
+        backbone: 'lib/backbone',
+
+        //
+        'AppView' : "test/views"
+
+    }
+});
+require(['AppView'], function (AppView) {
+    new AppView;
+});
