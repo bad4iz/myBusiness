93de1b6643be3b030dff644ad80fa8914ec77bf4
bad4iz <bad4iz@gmail.com>
2017-02-13 19:06:14 +0400

    require PeopleCollection

--- src/js/test/collections/imageCollection.js
+++ src/js/test/collections/imageCollection.js
@@ -1,3 +1,11 @@
 /**
  * Created by bad4iz on 13.02.2017.
  */
+define(['backbone', 'Person'], function (Backbone, Person) {
+
+    var PeopleCollection = Backbone.Collection.extend({
+        model: Person
+    });
+
+    return PeopleCollection;
+});
