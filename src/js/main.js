2f1f76503b4efe7c62ea6caab9900bbc7886cf82
bad4iz <bad4iz@gmail.com>
2017-02-14 05:58:25 +0400

    42

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,3 @@
-// (function () {
-//     window.App = {
-//         Models: {},
-//         Views: {},
-//         Collections: {},
-//         Router: {}
-//     };
-//
-//     // хелпер шаблона
-//     window.template = function (id) {
-//         return _.template($('#' + id).html());
-//     };
-//
-// }());
-
-
 require.config({
     paths: {
         jquery: "lib/jquery-1.8.3",
